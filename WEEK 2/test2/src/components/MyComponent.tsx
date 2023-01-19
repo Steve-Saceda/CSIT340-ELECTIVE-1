487e5f8bfd4dc1f0a7e6a3a86a00e65e91e8b282
Steve Wilben Saceda <94582096+Steve-Saceda@users.noreply.github.com>
2023-01-19 11:45:52 +0800

    Add files via upload

---
+++ WEEK 2/test2/src/components/MyComponent.tsx
@@ -0,0 +1,16 @@
+type MyCompType = {
+    name: string;
+    description: string;
+    numericvar?: number;
+}
+
+export default function MyComp(props:MyCompType) {
+    return (
+        <div>
+            <h2>Hi</h2>
+            <p>{Math.random() * 100}</p>
+            <p>{props.name}</p>
+            <p>{props.description}</p>
+        </div>
+    )
+}
